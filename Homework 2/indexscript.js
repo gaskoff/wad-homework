d99bef0362aab43f704ff2e6e8358efacf9f9cd6
Mikk Gaškov <gaskoff@ut.ee>
2021-10-24 22:44:57 +0300

    user-info css, createPostHTML koodi korduse kaotamine

--- Homework 2/indexscript.js
+++ Homework 2/indexscript.js
@@ -1,4 +1,21 @@
 function createPostHTML(id, author, title, body, datetime, image) {
+  let postHTML = '';
+  postHTML += '<div class="post" id="' + id + '"> \
+                 <div class="post-header"> \
+                   <img src="img/pea.png" /> \
+                   <span class="date-time">' + datetime + '</span> \
+                </div> \
+                <div class="post-content">';
+  if(image!=""){
+    postHTML += '<img src='+image+' alt="Image">';
+  }
+  postHTML += '  <h1>' +title+'</h1>\
+                 <p>' + body + '</p> \
+               </div> \
+               <div class="post-footer"> <img src="img/thumb.png" /></div> \
+               </div>';
+  return postHTML;
+  /*
     if(image!=""){
         return '<div class="post" id="' + id + '"> \
       <div class="post-header"> \
@@ -25,20 +42,27 @@ function createPostHTML(id, author, title, body, datetime, image) {
       </div> \
       <div class="post-footer"> <img src="img/thumb.png" /></div> \
       </div>';
-    }
+    }*/
   }
-  $.getJSON("https://api.jsonbin.io/b/61741d609548541c29c76bf2/3", function(result){
-    $.each(result, function(i, field){
-      let postID = "post-" + field.id;
-      let postDateTime = field.create_time;
-      let postBody = field.body;
-      let postAuthor = field.author;
-      let postTitle = field.title;
-      let postImg = field.img;
-      $("main").append(createPostHTML(postID, postAuthor, postTitle, postBody, postDateTime, postImg));
-    });
+
+$.getJSON("https://api.jsonbin.io/b/61741d609548541c29c76bf2/3", function(result){
+  $.each(result, function(i, field){
+    let postID = "post-" + field.id;
+    let postDateTime = field.create_time;
+    let postBody = field.body;
+    let postAuthor = field.author;
+    let postTitle = field.title;
+    let postImg = field.img;
+    $("main").append(createPostHTML(postID, postAuthor, postTitle, postBody, postDateTime, postImg));
   });
+});
 
 
-  
-  
+// Homework 2 task 7 logged-in user dropdown
+function toggleUserInfo(){
+  let display = document.getElementById("user-info").style.display;
+  document.getElementById("user-info").style.display = display !== "block" ? "block" : "none";
+}
+$("#user").click(function(){
+  toggleUserInfo();
+});
