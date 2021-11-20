fd0aee5ea4412ba66f8b9ef3414565adfce56ba8
Mikk Gaškov <gaskoff@ut.ee>
2021-11-20 15:23:58 +0200

    created inital vue project for homework3

---
+++ homework3/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
+    <h1>öojssdhgpjöosfdoangdsfg</h1>
+  </div>
+</template>
+
+<style lang="scss">
+
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background-color: yellow;
+  /*display: flex;
+  align-items: center;
+  flex-direction: column;*/
+}
+
+#nav {
+
+  background-color: pink;
+
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
