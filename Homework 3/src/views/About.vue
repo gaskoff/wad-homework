6e2e1df59e6b5576b51b531df3768cbfa2863282
KarlMagnusLaikoja <56919597+KarlMagnusLaikoja@users.noreply.github.com>
2021-11-21 13:53:57 +0200

    contact us page content+styling, about us page content+styling

--- Homework 3/src/views/About.vue
+++ Homework 3/src/views/About.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
-    <p>lkdffgdfgn</p>
+    <h1>Who we are:</h1>
+    <h2>We are Karl-Magnus Laikoja, Mikk Gaškov and Henri Suokas from Group 1</h2>
+    <img src=https://freedesignfile.com/upload/2018/08/Three-Business-People-Stock-Photo-01.jpg>
+    <p>Disclaimer: we are not actually these people.</p>
   </div>
 </template>
 
@@ -9,4 +11,9 @@
 .about {
   background-color: orange;
 }
+h1{
+  background-color: rgb(92, 226, 98);
+  border-style: solid;
+}
+
 </style>
--- Homework 3/src/views/Contactus.vue
+++ Homework 3/src/views/Contactus.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="contactus">
-    <h1>This is a contact us page</h1>
+    <h1>Contact us at:</h1>
+    <div class = contactdetails>
+      <h2>Karl-Magnus Laikoja</h2>
+      <h3>Email: karl.magnus.laikoja@gmail.com</h3>
+      <h3>Phone: 56202737</h3>
+    </div>
+    <div class = contactdetails>
+      <h2>Mikk Gaškov</h2>
+      <h3>Email: email</h3>
+      <h3>Phone: phone</h3>
+    </div>
+    <div class = contactdetails>
+      <h2>Henri Suokas</h2>
+      <h3>Email: email</h3>
+      <h3>Phone: phone</h3>
+    </div>
   </div>
 </template>
 
@@ -8,4 +23,9 @@
 .contactus {
   background-color: yellow;
 }
+.contactdetails{
+  background-color: grey;
+  border-style: solid;
+}
+
 </style>
