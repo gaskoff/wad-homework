7df107a234fe72f987075d474d1330b74f05e5a4
Mikk Gaškov <gaskoff@ut.ee>
2021-11-21 22:03:06 +0200

    katsetused

--- Homework 3/src/components/Mainbody.vue
+++ Homework 3/src/components/Mainbody.vue
@@ -2,7 +2,8 @@
   <div id="mainbody">
     <h1>Home</h1>
     <button @click="reset">Reset likes</button>
-    <Post1 ref="a"/>
+    <Post />
+<!--    <Post1 ref="a"/>
     <Post2 ref="b"/>
     <Post3 ref="c"/>
     <Post4 ref="d"/>
@@ -11,12 +12,17 @@
     <Post7 ref="g"/>
     <Post8 ref="h"/>
     <Post9 ref="i"/>
-    <Post10 ref="j"/>
+    <Post10 ref="j"/> -->
   </div>
 
 </template>
 
 <script>
+
+import Post from "@/components/Post.vue";
+import { mapGetters } from "vuex"
+
+/*
 import Post1 from "@/components/Post1.vue";
 import Post2 from "@/components/Post2.vue";
 import Post3 from "@/components/Post3.vue";
@@ -27,22 +33,19 @@ import Post7 from "@/components/Post7.vue";
 import Post8 from "@/components/Post8.vue";
 import Post9 from "@/components/Post9.vue";
 import Post10 from "@/components/Post10.vue";
+*/
 export default {
   name: "Mainbody",
-  
-  components: { Post1, Post2, Post3, Post4, Post5, Post6, Post7, Post8, Post9, Post10 },
+  components: { Post },
+  computed: {
+    ...mapGetters(["getPosts"])
+  },
   methods: {
     reset (){
-      this.$refs.a.reset();
-      this.$refs.b.reset();
-      this.$refs.c.reset();
-      this.$refs.d.reset();
-      this.$refs.e.reset();
-      this.$refs.f.reset();
-      this.$refs.g.reset();
-      this.$refs.h.reset();
-      this.$refs.i.reset();
-      this.$refs.j.reset();
+      console.log('midagi toimub');
+      this.getPosts.resetLikeCount;
+      console.log('midagi toimub 2');
+
     }
   }
 };
