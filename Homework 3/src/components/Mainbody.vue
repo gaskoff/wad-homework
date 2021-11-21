b1c7a0cbf117cc1cd6af688a52f22fd82608db1e
Mikk Gaškov <gaskoff@ut.ee>
2021-11-21 15:00:31 +0200

    postitused loetakse store'ist, <img src> ei tööta

--- Homework 3/src/components/Mainbody.vue
+++ Homework 3/src/components/Mainbody.vue
@@ -1,18 +1,37 @@
 <template>
   <div id="mainbody">
     <h1>main body with posts</h1>
-    <Post />
+
+    <h2>dsfhsghdfh</h2>
+    <div class="post" v-for="post in posts" :key="post.id">
+      <div class="post-header">
+        <img src="@/assets/pea.png" />
+        <span class="date-time">{{post.create_time}}</span>
+      </div>
+      <div class="post-content">
+        <img src={{post.img}} />
+        <h1>{{post.title}}</h1>
+        <p>{{post.body}} </p>
+
+      </div>
+      <div class="post-footer"> <img src="@/assets/thumb.png" /></div>
+    </div>
     <p>spskdjffsdf</p>
   </div>
 
 </template>
 
 <script>
-import Post from "@/components/Post.vue";
+/*import Post from "@/components/Post.vue";*/
 export default {
   name: "Mainbody",
-  components: { Post }
-};
+  /*components: { Post }*/
+
+  computed: {
+    posts() {
+      return this.$store.state.posts
+  }}
+}
 </script>
 
 <style lang="scss">
@@ -22,6 +41,43 @@ export default {
   flex-direction: column;
   align-items: center;
   background-color: white;
+}
+
+.post{
+  width: 60%;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  margin: 10px;
+  border-radius: 2px;
+}
+.date-time{
+  font-size: x-small;
+  font-family: Verdana, sans-serif;
+  color: gray;
+}
+
+.post-header{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 5px;
+  background-color: white;
+}
+
+.post-header > img, .post-footer > img{
+  height: 25px;
+}
+
+.post-content{
+  margin: 5px;
+}
+
+.post-content > img{
+  width: 100%;
+}
+
+.post-content > h1{
+  font-size: 1em;
+  text-transform: uppercase;
 }
 
 </style>
--- Homework 3/src/store/index.js
+++ Homework 3/src/store/index.js
@@ -1,8 +1,98 @@
 import { createStore } from "vuex";
 
-export default createStore({
+/*export default createStore({
   state: {},
   mutations: {},
   actions: {},
   modules: {},
+});
+*/
+export default createStore({
+    state: {
+        posts: [
+          {
+            "id": 1,
+            "title": "I think it's going to rain",
+            "body": "",
+            "img": "https://gamingbolt.com/wp-content/uploads/2017/08/Half-Life-2.jpg",
+            "author": "Alfred",
+            "create_time": "01.01.1970 13:24"
+          },
+          {
+            "id": 2,
+            "title": "Which weighs more, a pound of featers or a pound of bricks?",
+            "body": "",
+            "img": "",
+            "author": "Alfonso",
+            "create_time": "05.01.1970 13:24"
+          },
+          {
+            "id": 3,
+            "title": "Felt cute, might delete later",
+            "body": "",
+            "img": "https://i.ytimg.com/vi/mUUev-khwKs/maxresdefault.jpg",
+            "author": "Anna",
+            "create_time": "02.01.1970 13:24"
+          },
+          {
+            "id": 4,
+            "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+            "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+            "img": "",
+            "author": "Beth",
+            "create_time": "15.11.2020 19:24"
+          },
+          {
+            "id": 5,
+            "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+            "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+        "img": "",
+            "author": "Bertha",
+            "create_time": "07.07.2021 14:47"
+          },
+          {
+            "id": 6,
+            "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+            "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+        "img": "",
+            "author": "Cathy",
+            "create_time": "24.10.2020 18:24"
+          },
+          {
+            "id": 7,
+            "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+            "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+        "img": "",
+            "author": "Diego",
+            "create_time": "21.09.2021 16:24"
+          },
+          {
+            "id": 8,
+            "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+            "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+        "img": "",
+            "author": "Diana",
+            "create_time": "17.12.2017 19:13"
+          },
+          {
+            "id": 9,
+            "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+            "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+        "img": "",
+            "author": "Elizabeth",
+            "create_time": "12.10.2021 23:15"
+          },
+          {
+            "id": 10,
+            "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+            "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+        "img": "",
+            "author": "Zorro",
+            "create_time": "01.01.2007 12:00"
+          }
+        ]
+    },
+    mutations: {},
+    actions: {},
+    modules: {},
 });
