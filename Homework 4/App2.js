f44521fa185fdd60c8b715e7b6082d3aed006dd2
Mikk Gaškov <gaskoff@ut.ee>
2021-12-09 04:05:12 +0200

    alustuseks minu poolt midagi...

--- Homework 4/App2.js
+++ Homework 4/App2.js
@@ -2,6 +2,7 @@ const express = require('express');
 const pool = require('./database');
 const cors = require('cors');
 const app = express();
+
 // register the ejs view engine
 app.set('view engine', 'ejs');
 //without this middleware, we cannot use data submitted by forms
@@ -9,44 +10,52 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 app.use(express.static('Public'));
+
 app.listen(3000, () => {
  console.log("Server is listening to port 3000")
 });
+
 app.get('/', async(req, res) => {
  try {
  console.log("get posts request has arrived");
  const posts = await pool.query(
- "SELECT * FROM Posts"
+   // "SELECT * FROM Posts"
+   "SELECT * FROM nodetable"
  );
  res.render('posts', { posts: posts.rows });
  } catch (err) {
  console.error(err.message);
  }
 });
+
 app.get('/singleposts/:id', async(req, res) => {
  try {
  const id = req.params.id;
  console.log(req.params.id);
  console.log("get a single post request has arrived");
  const posts = await pool.query(
- "SELECT * FROM Posts WHERE id = $1", [id]
+ //"SELECT * FROM Posts WHERE id = $1", [id]
+  "SELECT * FROM nodetable WHERE id = $1", [id]
  );
  res.render('singleposts', { posts: posts.rows[0] });
  } catch (err) {
  console.error(err.message);
  }
 });
+
 app.get('/:id', async(req, res) => {
  try {
  const { id } = req.params;
  console.log("get a post request has arrived");
  const Apost = await pool.query(
- "SELECT * FROM Posts WHERE id = $1", [id]
+ //"SELECT * FROM Posts WHERE id = $1", [id]
+ "SELECT * FROM nodetable WHERE id = $1", [id]
  );
  res.json(Apost.rows[0]);
  } catch (err) { console.error(err.message);
  }
 });
+
 app.delete('/:id', async(req, res) => {
  try {
  console.log(req.params);
@@ -54,13 +63,15 @@ app.delete('/:id', async(req, res) => {
  const post = req.body;
  console.log("delete a post request has arrived");
  const deletepost = await pool.query(
- "DELETE FROM Posts WHERE id = $1", [id]
+ //"DELETE FROM Posts WHERE id = $1", [id]
+ "DELETE FROM nodetable WHERE id = $1", [id]
  );
  res.redirect('posts');
  } catch (err) {
  console.error(err.message);
  }
 });
+
 /*app.post('/', async(req, res) => {
  try {
  const post = req.body;
@@ -77,6 +88,7 @@ RETURNING*", [post.title, post.body, post.urllink]
 app.get('/create', (req, res) => {
  res.render('create');
 });*/
+
 app.use((req, res) => {
  res.status(404).render('404');
-}); 
+});
--- Homework 4/database.js
+++ Homework 4/database.js
@@ -1,9 +1,13 @@
 const Pool = require('pg').Pool;
 const pool = new Pool({
  user: "postgres",
- password: "password",
- database: "database",
- host: "localhost",
+ //password: "password",
+ password: "********",
+ //database: "database",
+ database: "postgres",
+ //host: "localhost",
+ host: "217.146.78.152",
+
  port: "5432"
 });
 module.exports = pool;
