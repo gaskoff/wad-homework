3eb3177316628c36f477d73450a6b40f6e808588
Mikk Gaškov <gaskoff@ut.ee>
2021-12-11 22:42:06 +0200

    changed post timestamp generation

--- Homework 4/App2.js
+++ Homework 4/App2.js
@@ -121,10 +121,12 @@ app.post('/addnewpost', async(req, res) => {
      let postsCount = postsCountQuery.rows[0].max;
      console.log(postsCount);
 
+     const now = new Date().toLocaleString();
+
      // post id will be one higher than the highest id of posts in db
      const newpost = await pool.query(
        "INSERT INTO nodetable(id, title, body, urllink, likes_count, author, timestamp) VALUES ($1, $2, $3, $4, $5, $6, $7)",
-       [++postsCount, post.title, post.body, post.urllink, 0, post.author, post.timestamp]
+       [++postsCount, post.title, post.body, post.urllink, 0, post.author, now]
      );
 
      // redirectig to first page with all the posts seems logical
